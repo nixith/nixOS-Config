06d332272701dc3cf7406ffc8a06c8eb5dcc95ee
arcolinuxz <arcolinuxinfo@gmail.com>
2022-12-20 16:22:35 -0500

    actually move packages in a proper format

--- modules/packages.nix
+++ modules/packages.nix
@@ -1,4 +1,5 @@
-
+{pkgs, ...}:
+{
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
@@ -10,3 +11,4 @@
     gh
     btop
   ];
+}
