8d5aa821b919b264626b93325312c4a7d779d6f5
nixith <rwstauffer72@gmail.com>
2023-03-04 10:41:41 -0500

    comment out docker

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -39,8 +39,11 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
+
+  #Docker
+  #virtualisation.docker.enable = true;
+  #virtualisation.docker.storageDriver = "btrfs";
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
