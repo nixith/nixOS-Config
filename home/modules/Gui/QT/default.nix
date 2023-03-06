bf03e3466c9f30bfc025eac95ec004f06c8eb0ae
bromine1 <bromine1>
2023-03-05 20:39:59 -0500

    update QT module

--- home/modules/Gui/QT/default.nix
+++ home/modules/Gui/QT/default.nix
@@ -2,6 +2,10 @@
 {
   home.packages = with pkgs;
     [
+      qt6.full
+      qt6.qtwayland
+      qt6.wrapQtAppsHook
+      qt5.qtwayland
       libsForQt5.qt5ct
       libsForQt5.qtstyleplugin-kvantum
       libsForQt5.qt5.qtwayland
@@ -9,26 +13,27 @@
     ];
   qt = {
     enable = true;
-    style = {
-      package = pkgs.libsForQt5.qtstyleplugin-kvantum;
-      name = "kvantum";
-    };
+    platformTheme = "gtk";
+    #style = {
+    #  package = pkgs.libsForQt5.qtstyleplugin-kvantum;
+    #  name = "kvantum";
+    #};
   };
 
-  home.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_STYLE_OVERRIDE = "kvantum";
-  };
+  #  home.sessionVariables = {
+  #    QT_QPA_PLATFORMTHEME = "qt5ct";
+  #    QT_STYLE_OVERRIDE = "kvantum";
+  #  };
 
-  xdg.dataFile = {
-    "./kvantumThemes/Catppuccin-Mocha-Lavender" = {
-      source = ./kvantumThemes/Catppuccin-Mocha-Lavender;
-    };
-  };
-  xdg.configFile = {
-    "./qt5ct/catppuccin.conf" = {
-      source = ./qt5ct/catppuccin.conf;
-      target = "qt5ct/catppuccin.conf";
-    };
-  };
+  #  xdg.dataFile = {
+  #    "./kvantumThemes/Catppuccin-Mocha-Lavender" = {
+  #      source = ./kvantumThemes/Catppuccin-Mocha-Lavender;
+  #    };
+  #  };
+  #  xdg.configFile = {
+  #    "./qt5ct/catppuccin.conf" = {
+  #      source = ./qt5ct/catppuccin.conf;
+  #      target = "qt5ct/catppuccin.conf";
+  #    };
+  #  };
 }
