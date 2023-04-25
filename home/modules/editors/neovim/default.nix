261c938b7cc7b5e5a834e266aaf9dec911492078
nix <rwstauffer72@gmail.com>
2023-04-24 22:51:22 -0400

    add lazy.nvim config

--- home/modules/editors/neovim/default.nix
+++ home/modules/editors/neovim/default.nix
@@ -1,10 +1,25 @@
-{ pkgs, ... }: {
+{
+  pkgs,
+  system,
+  alejandra,
+  ...
+}: {
+  # move config files to .config
+
+  home.file.nvim = {
+    enable = true;
+    recursive = true;
+    source = ./nvim;
+    target = ".config/nvim";
+  };
+
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-nightly;
     withPython3 = true;
     withNodeJs = true;
   };
+
   home.packages = with pkgs; [
     ripgrep
     lazygit
@@ -18,15 +33,19 @@
     tree-sitter
     libstdcxx5
 
-  #Python
+    #Python
     # Linter
     ruff
 
     # LSP
     python310Packages.jedi-language-server
     nodePackages_latest.pyright
-    luajitPackages.lua-lsp
+    nodePackages_latest.vscode-json-languageserver
+    sumneko-lua-language-server
+    stylua
     jdt-language-server
-  ];
 
+    # Formatter
+    alejandra.defaultPackage.${system}
+  ];
 }
