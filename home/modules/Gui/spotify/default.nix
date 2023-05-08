34d61bf93c7a2e770aa0f0f73cd4e764dad61d6d
nixith <rwstauffer72@gmail.com>
2023-05-08 07:15:07 -0400

    spotifyd

--- home/modules/Gui/spotify/default.nix
+++ home/modules/Gui/spotify/default.nix
@@ -1,5 +1,32 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [spotify-tui];
 
-  programs.spotifyd = {};
+  services.spotifyd = {
+    enable = true;
+    settings = {
+      global = {
+        audio_format = "S16";
+        autoplay = true;
+        backend = "pulseaudio";
+        bitrate = 320;
+        cache_path = "cache_directory";
+        dbus_type = "session";
+        device = "default";
+        device_name = "nixos";
+        device_type = "computer";
+        initial_volume = "90";
+        max_cache_size = 1000000000;
+        mixer = "PCM";
+        no_audio_cache = true;
+        normalisation_pregain = -10;
+        on_song_change_hook = "command_to_run_on_playback_events";
+        # proxy = "http://proxy.example.org:8080";
+        use_keyring = true;
+        use_mpris = true;
+        volume_controller = "alsa";
+        volume_normalisation = true;
+        # zeroconf_port = 1234;
+      };
+    };
+  };
 }
