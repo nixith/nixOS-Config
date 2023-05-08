d40733cfe9f8f68f159c8c0e8eda59a54f8d1d4c
nix <rwstauffer72@gmail.com>
2023-05-07 21:48:45 -0400

    reenable pulse

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -51,7 +51,7 @@
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    #pulse.enable = true;
+    pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
 
