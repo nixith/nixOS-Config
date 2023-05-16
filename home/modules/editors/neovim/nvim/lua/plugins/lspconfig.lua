f58b596f8df5fbbcca15ed3b0bebe0d0622099a3
nixith <rwstauffer72@gmail.com>
2023-05-16 13:36:05 -0400

    add some python config

--- home/modules/editors/neovim/nvim/lua/plugins/lspconfig.lua
+++ home/modules/editors/neovim/nvim/lua/plugins/lspconfig.lua
@@ -41,7 +41,9 @@ return {
             },
           },
         },
-        -- python
+        --python
+        ruff_lsp = {},
+        jedi_language_server = {},
       },
       -- you can do any additional lsp server setup here
       -- return true if you don't want this server to be setup with lspconfig
