381ca5d6eaa712c947b3a9fbc1296f6a7ff4df50
nix <rwstauffer72@gmail.com>
2023-07-05 20:02:58 -0400

    add btrfs install script

---
+++ scripts/btrfs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+printf "label: gpt\n,550M,U\n,,L\n" | sfdisk "/dev/$1"
+
+mkfs.fat -F 32 "/dev/$1"1
+
+mkfs.btrfs "/dev/$1"2
+mkdir -p /mnt
+mount "/dev/$1"2 /mnt
+btrfs subvolume create /mnt/root
+btrfs subvolume create /mnt/home
+btrfs subvolume create /mnt/nix
+umount /mnt
+
+mount -o compress=zstd,subvol=root "/dev/$1"2 /mnt
+mkdir /mnt/{home,nix}
+mount -o compress=zstd,subvol=home "/dev/$1"2 /mnt/home
+mount -o compress=zstd,noatime,subvol=nix "/dev/$1"2 /mnt/nix
+
+mkdir /mnt/boot
+mount /dev/"$1"1 /mnt/boot
+
+nixos-install --flake .#"$2"
