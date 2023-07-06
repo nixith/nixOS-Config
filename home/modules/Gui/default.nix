20df564f9fb959c2b9b5cc870a43897c288d21e0
nix <rwstauffer72@gmail.com>
2023-07-05 20:02:11 -0400

    patch prism launcher with glfw-wayland

--- home/modules/Gui/default.nix
+++ home/modules/Gui/default.nix
@@ -6,58 +6,56 @@
   inputs,
   nixpkgs,
   ...
-}: {
+}: let
+  prismlauncher = pkgs.prismlauncher.override {glfw = pkgs.glfw-wayland;};
+in {
   nixpkgs.config.allowUnfree = true;
   # Apps without Home Manager Modules
-  home.packages = with pkgs; [
-    # note taking
-    xournalpp
-    logseq
+  home.packages = with pkgs;
+    [
+      # note taking
+      xournalpp
+      logseq
 
-    # Gaming
-    #steamcmd
-    #steam-tui
-    mesa
-    dxvk
-    #itch #(commented due to InsecurePackage)
-    #prismlauncher.packages.${pkgs.system}.prismlauncher
-    prismlauncher
+      # Gaming
+      #steamcmd
+      #steam-tui
+      mesa
+      dxvk
+      #itch #(commented due to InsecurePackage)
+      #prismlauncher.packages.${pkgs.system}.prismlauncher
 
-    # Utilities
-    cinnamon.nemo-with-extensions
-    flameshot
-    carla
-    csa
-    calf
-    rnnoise-plugin # Denoiser plugin
-    distrho
-    cadence
-    noisetorch
-    pavucontrol
-    gimp
-    playerctl
-    # bluetooth
-    bluez
-    bluez-tools
-    blueberry
+      # Utilities
+      cinnamon.nemo-with-extensions
+      flameshot
+      carla
+      csa
+      calf
+      rnnoise-plugin # Denoiser plugin
+      distrho
+      cadence
+      noisetorch
+      pavucontrol
+      gimp
+      playerctl
+      # bluetooth
+      bluez
+      bluez-tools
+      blueberry
 
-    # Productivity
-    libreoffice
-    calibre
+      # Productivity
+      libreoffice
+      calibre
 
-    # Web
-    firefox-wayland
-    brave
-    element-desktop
+      # Media
+      mpv
+      imv
 
-    # Media
-    mpv
-    imv
+      #art
+      pixelorama
 
-    #art
-    pixelorama
-
-    #Libs
-    xorg.libxcb
-  ];
+      #Libs
+      xorg.libxcb
+    ]
+    ++ [prismlauncher];
 }
