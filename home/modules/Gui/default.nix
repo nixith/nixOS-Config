749e02c533bc3f24d975ae00f20c6449e90011e7
nix <rwstauffer72@gmail.com>
2023-07-25 08:06:59 -0400

    stop compiling prismlauncher

--- home/modules/Gui/default.nix
+++ home/modules/Gui/default.nix
@@ -7,55 +7,54 @@
   nixpkgs,
   ...
 }: let
-  prismlauncher = inputs.prismlauncher.packages.${pkgs.system}.prismlauncher.override {glfw = pkgs.glfw-wayland;};
+  #prismlauncher = inputs.prismlauncher.packages.${pkgs.system}.prismlauncher.override {glfw = pkgs.glfw-wayland;};
 in {
   nixpkgs.config.allowUnfree = true;
   # Apps without Home Manager Modules
-  home.packages = with pkgs;
-    [
-      # note taking
-      xournalpp
-      logseq
+  home.packages = with pkgs; [
+    # note taking
+    xournalpp
+    logseq
 
-      # Gaming
-      #steamcmd
-      #steam-tui
-      mesa
-      dxvk
-      #itch #(commented due to InsecurePackage)
-      #prismlauncher.packages.${pkgs.system}.prismlauncher
+    # Gaming
+    #steamcmd
+    #steam-tui
+    mesa
+    dxvk
+    #itch #(commented due to InsecurePackage)
+    #prismlauncher.packages.${pkgs.system}.prismlauncher
 
-      # Utilities
-      cinnamon.nemo-with-extensions
-      flameshot
-      carla
-      csa
-      calf
-      rnnoise-plugin # Denoiser plugin
-      distrho
-      cadence
-      noisetorch
-      pavucontrol
-      gimp
-      playerctl
-      # bluetooth
-      bluez
-      bluez-tools
-      blueberry
+    # Utilities
+    cinnamon.nemo-with-extensions
+    flameshot
+    carla
+    csa
+    calf
+    rnnoise-plugin # Denoiser plugin
+    distrho
+    cadence
+    noisetorch
+    pavucontrol
+    gimp
+    playerctl
+    # bluetooth
+    bluez
+    bluez-tools
+    blueberry
 
-      # Productivity
-      libreoffice
-      calibre
+    # Productivity
+    libreoffice
+    calibre
 
-      # Media
-      mpv
-      imv
+    # Media
+    mpv
+    imv
 
-      #art
-      pixelorama
+    #art
+    pixelorama
 
-      #Libs
-      xorg.libxcb
-    ]
-    ++ [prismlauncher];
+    #Libs
+    xorg.libxcb
+  ];
+  #++ [prismlauncher];
 }
