892823ee985f8da70c1fd033b8e48d82bc46eb66
bromine1 <rwstauffer72@gmail.com>
2023-09-12 11:02:28 -0400

    swap to skim

--- home/modules/CLI/Tools.nix
+++ home/modules/CLI/Tools.nix
@@ -6,28 +6,49 @@
   user,
   inputs,
   ...
-}: {
+}: let
+  skimColors = "--color=fg:#cdd6f4,bg:#1e1e2e,matched:#313244,matched_bg:#f2cdcd,current:#cdd6f4,current_bg:#45475a,current_match:#1e1e2e,current_match_bg:#f5e0dc,spinner:#a6e3a1,info:#cba6f7,prompt:#89b4fa,cursor:#f38ba8,selected:#eba0ac,header:#94e2d5,border:#6c7086";
+in {
   programs = {
     bat.enable = true; # cat clone
-    fzf = {
+    skim = {
       enable = true;
-      colors = {
-        "bg+" = "#313244";
-        bg = "#1e1e2e";
-        spinner = "#f5e0dc";
-        hl = "#f38ba8";
-        fg = "#cdd6f4";
-        header = "#f38ba8";
-        info = "#cba6f7";
-        pointer = "#f5e0dc";
-        arker = "#f5e0dc";
-        "fg+" = "#cdd6f4";
-        prompt = "#cba6f7";
-        "hl+" = "#f38ba8";
-      };
+      changeDirWidgetCommand = ''
+        fd . --type d --follow --exclude '*.direnv/*'
+      '';
+      changeDirWidgetOptions = [
+        "--preview 'eza {} -F --icons --color=always -T -L 4'"
+        skimColors
+      ];
+      fileWidgetCommand = ''
+        fd . --type f --follow --exclude '*.direnv/*'
+      '';
+      fileWidgetOptions = [
+        "--preview 'bat {} --paging never --color always'"
+        skimColors
+      ];
+
+      defaultCommand = ''
+        fd . --type d --follow --exclude '*.direnv/*'
+      '';
+
+      defaultOptions = [
+        skimColors
+      ];
     };
-    zathura.enable = true;
+
+    eza = {
+      enable = true;
+      enableAliases = true;
+      extraOptions = [
+        "-F"
+        "--header"
+      ];
+      icons = true;
+      git = true;
+    };
   };
+
   home.packages = with pkgs; [
     coreutils-full
     #  Archive Tools
