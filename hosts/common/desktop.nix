92944157f79539e0584ecf31f4698e043a770c02
bromine1 <rwstauffer72@gmail.com>
2023-09-17 10:34:13 -0400

    setup some printing stuff

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -59,6 +59,10 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  # for a WiFi printer
+  services.avahi.openFirewall = true;
 
   # Enable flatpak
   xdg.portal = {
