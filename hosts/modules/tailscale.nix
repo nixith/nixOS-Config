8fd73abf32dc0ba05f26afe8528cd54783e002cd
bromine1 <rwstauffer72@gmail.com>
2023-10-06 18:05:49 -0400

    add tailscale module

---
+++ hosts/modules/tailscale.nix
@@ -0,0 +1,13 @@
+{config, ...}: {
+  services = {
+    tailscale = {
+      useRoutingFeatures = "both";
+      enable = true;
+    };
+  };
+  networking.firewall = {
+    enable = true;
+    trustedInterfaces = ["tailscale0"];
+    allowedUDPPorts = [config.services.tailscale.port];
+  };
+}
