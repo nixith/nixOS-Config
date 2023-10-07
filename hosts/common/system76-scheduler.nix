be14a09ec514a39bd8893f86f9215e5304af2813
bromine1 <rwstauffer72@gmail.com>
2023-10-07 14:16:29 -0400

    change some system 76 scheduler items

--- hosts/common/system76-scheduler.nix
+++ hosts/common/system76-scheduler.nix
@@ -6,5 +6,12 @@
   services.system76-scheduler = {
     enable = true;
     useStockConfig = true;
+    settings = {
+      processScheduler = {
+        pipewireBoost.enable = true;
+        foregroundBoost = true;
+      };
+      cfsProfiles.enable = true;
+    };
   };
 }
