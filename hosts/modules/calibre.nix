d3ec3d8db2a9da839f896ba53cef45aa4b9cb344
bromine1 <rwstauffer72@gmail.com>
2023-10-07 14:16:38 -0400

    fix calibre module

--- hosts/modules/calibre.nix
+++ hosts/modules/calibre.nix
@@ -1,13 +1,20 @@
-{pkgs, ...}: {
+{config, ...}: {
   services.calibre-web = {
     enable = true;
+    listen = {
+      ip = "0.0.0.0";
+    };
     options = {
       calibreLibrary = /home/ryan/CalibreLibrary;
       enableKepubify = true;
       enableBookUploading = true;
     };
   };
-  #services.calibre-server = {
-  #  enable = true;
-  #};
+  services.calibre-server = {
+    enable = true;
+    user = "ryan";
+    libraries = [
+      /home/ryan/CalibreLibrary
+    ];
+  };
 }
