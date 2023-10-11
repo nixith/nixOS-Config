1d6f8929636bc8be01587af930f149861c58e605
bromine1 <rwstauffer72@gmail.com>
2023-10-10 20:59:09 -0400

    remove embedded import - handle at flake level

--- home/modules/Gui/spotify/default.nix
+++ home/modules/Gui/spotify/default.nix
@@ -6,8 +6,6 @@
 }: let
   spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
 in {
-  imports = [inputs.spicetify-nix.homeManagerModule];
-
   programs.spicetify = {
     enable = true;
     theme = {
