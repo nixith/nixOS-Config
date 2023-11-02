91c6d34da2c7f889315f307703eedda8282ab817
bromine1 <rwstauffer72@gmail.com>
2023-11-02 19:19:17 -0400

    add typst support for tree-sitter

--- home/modules/editors/neovim/nvim/lua/plugins/tree-sitter.lua
+++ home/modules/editors/neovim/nvim/lua/plugins/tree-sitter.lua
@@ -14,17 +14,18 @@ return {
           return true
         end, opts.ensure_installed)
       end
+      require("nvim-treesitter.install").prefer_git = true
       require("nvim-treesitter.configs").setup(opts)
 
       -- Here's my own stuff
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
       parser_config.typst = {
         install_info = {
-          url = "https://github.com/uben0/tree-sitter-typst.git", -- local path or git repo
-          files = { "src/parser.c" "src/scanner.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+          url = "https://github.com/uben0/tree-sitter-typst", -- local path or git repo
+          files = { "src/parser.c", "src/scanner.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
           -- optional entries:
-          branch = "main", -- default branch in case of git repo if different from master
-          requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+          generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+          requires_generate_from_grammar = true,
         },
         filetype = "typst", -- if filetype does not match the parser name
       }
