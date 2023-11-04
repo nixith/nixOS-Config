9382c532a32258a90ba2d357e0a706d969ee930e
bromine1 <rwstauffer72@gmail.com>
2023-11-04 13:12:18 -0400

    add neotest

--- home/modules/editors/neovim/nvim/lua/config/lazy.lua
+++ home/modules/editors/neovim/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    { import = "lazyvim.test.core" },
 
     -- import/override with your plugins
     { import = "plugins" },
