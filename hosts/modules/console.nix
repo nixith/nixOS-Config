1c4cf1e12592cad99508f61d86ae4bcc6cc6b01c
bromine1 <rwstauffer72@gmail.com>
2023-11-11 12:37:10 -0500

    initial VT config

---
+++ hosts/modules/console.nix
@@ -0,0 +1,22 @@
+{...}: {
+  console = {
+    colors = [
+      "1e1e2e"
+      "45475a"
+      "f38ba8"
+      "eba0ac"
+      "94e2d5"
+      "a6e3a1"
+      "fab387"
+      "f9e2af"
+      "89b4fa"
+      "74c7ec"
+      "cba6f7"
+      "b4befe"
+      "89dceb"
+      "94e2d5"
+      "bac2de"
+      "cdd6f4"
+    ];
+  };
+}
