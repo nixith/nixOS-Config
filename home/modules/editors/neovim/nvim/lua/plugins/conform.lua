5528c9b62a6f32202a192c10e4a23e552c0a6ee2
nix <rwstauffer72@gmail.com>
2023-11-12 20:06:16 -0500

    add typstfmt

--- home/modules/editors/neovim/nvim/lua/plugins/conform.lua
+++ home/modules/editors/neovim/nvim/lua/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
         css = { "sylint" },
         toml = { "taplo" },
         yaml = { "yamlfmt" },
+        typst = { "typstfmt" },
         ["*sh"] = { { "shellcheck", "shellharden", "shfmt" } },
         -- ["*"] = { "injected" }, -- commented out so that LSP formatting will load
       },
