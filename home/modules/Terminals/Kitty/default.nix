ee709cb059e45e436295463ff97ec953295ffc53
bromine1 <rwstauffer72@gmail.com>
2024-03-30 10:50:02 -0400

    fix theme for Catppuccin

--- home/modules/Terminals/Kitty/default.nix
+++ home/modules/Terminals/Kitty/default.nix
@@ -10,11 +10,11 @@
       name = "Lilex Nerd Font Regular";
       size = 10;
     };
+    theme = "Catppuccin-Mocha";
     settings = {
       background_opacity = "0.4";
       background_tint = "1";
       shell = "fish --login --interactive";
     };
-    extraConfig = builtins.readFile ./catppuccin-mocha.conf;
   };
 }
