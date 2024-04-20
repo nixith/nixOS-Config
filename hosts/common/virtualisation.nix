0d79e685b1c78354e399832bdd4ea74abd996951
nixith <rwstauffer72@gmail.com>
2024-04-20 10:30:43 -0400

    add nvidia compat to containers

--- hosts/common/virtualisation.nix
+++ hosts/common/virtualisation.nix
@@ -1,9 +1,14 @@
 {
   pkgs,
   user,
+  config,
   ...
-}: {
+}: let
+  isNvidia = pkgs.lib.any (driver: driver == "nvidia") config.services.xserver.videoDrivers;
+in {
   virtualisation = {
+    # enable nvidia stuff for containers. Any CDI container spec supports this.
+    containers.cdi.dynamic.nvidia.enable = isNvidia;
     libvirtd = {
       enable = true;
       qemu = {
@@ -23,6 +28,10 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
+    dockerSocket.enable = true;
+    autoPrune = {
+      enable = true;
+    };
     defaultNetwork.settings.dns_enabled = true;
   };
   users.users.${user}.extraGroups = ["podman"];
