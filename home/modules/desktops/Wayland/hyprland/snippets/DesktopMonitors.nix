c8a8b664f483c8791bd28a489032582411fab865
nix <rwstauffer72@gmail.com>
2024-04-20 13:03:24 -0400

    enable vrr on fullscreen

--- home/modules/desktops/Wayland/hyprland/snippets/DesktopMonitors.nix
+++ home/modules/desktops/Wayland/hyprland/snippets/DesktopMonitors.nix
@@ -1,8 +1,8 @@
 {...}: ''
 
-  monitor=HDMI-A-1,1920x1080@60,0x0,1
-  monitor=DP-1,1920x1080@60,1920x0,1
-  monitor=DP-2,1920x1080@60,3840x0,1
+  monitor=HDMI-A-1,1920x1080@60,0x0,1,vrr,2
+  monitor=DP-1,1920x1080@60,1920x0,1,vrr,2
+  monitor=DP-2,1920x1080@60,3840x0,1,vrr,2
   monitor=DP-2,transform,1
   #monitor=DP-1,transform,1
 ''
