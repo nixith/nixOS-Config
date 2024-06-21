04ff20d21f5ca60bf39be9c30751b9c225398700
nix <rwstauffer72@gmail.com>
2024-06-21 11:09:34 -0400

    various nicities

--- home/modules/editors/neovim/nvim/lua/plugins/general.lua
+++ home/modules/editors/neovim/nvim/lua/plugins/general.lua
@@ -8,4 +8,27 @@ return {
     ft = "typst",
     --lazy = false,
   },
+
+  {
+    "ravibrock/spellwarn.nvim",
+    event = "VeryLazy",
+    config = true,
+  },
+
+  {
+    "MeanderingProgrammer/markdown.nvim",
+    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("render-markdown").setup({})
+    end,
+    ft = "markdown",
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+    end,
+    lazy = false,
+  },
 }
