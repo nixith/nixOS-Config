8cea2fb5f1256bb6caf9ba63a7ca4c0de50e329f
nixith <rwstauffer72@gmail.com>
2025-04-08 20:15:12 -0400

    use nixpkgs lix

--- hosts/default.nix
+++ hosts/default.nix
@@ -26,7 +26,7 @@ let
     ./modules/console.nix
     ./common/yubikey.nix
     flakeProgramsSqlite.nixosModules.programs-sqlite
-    inputs.lix-modules.nixosModules.default
+    inputs.lix-modules.nixosModules.lixFromNixpkgs
     ./modules/greetd.nix
     home-manager.nixosModules.default
     (
