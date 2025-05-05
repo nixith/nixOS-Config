241f22caf0849fd102a39097dd163e61d1331988
nix <rwstauffer72@gmail.com>
2025-05-05 12:32:52 -0400

    don't look for so many caches

--- hosts/common/system.nix
+++ hosts/common/system.nix
@@ -1,15 +1,9 @@
 # Edit this configuration file to define what should be installed oncon
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  inputs,
-  pkgs,
-  ...
-}:
+{ config, inputs, pkgs, ... }:
 let
-in
-{
+in {
   # NTFS Support
   boot.supportedFilesystems = [ "ntfs" ];
 
@@ -38,11 +32,7 @@ in
       extraConfig = ''
         ReadEtcHosts=yes
       '';
-      fallbackDns = [
-        "1.1.1.1"
-        "1.0.0.1"
-        "9.9.9.9"
-      ];
+      fallbackDns = [ "1.1.1.1" "1.0.0.1" "9.9.9.9" ];
     };
 
     fwupd.enable = true;
@@ -72,33 +62,17 @@ in
 
   nix = {
     settings = {
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://cache.garnix.io"
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://nix-gaming.cachix.org"
-        "https://anyrun.cachix.org"
-      ];
+      substituters =
+        [ "https://cache.nixos.org/" "https://nix-community.cachix.org" ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
 
       auto-optimise-store = true; # Optimise syslinks
       allow-import-from-derivation = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [ "root" "@wheel" ];
     };
     gc = {
       automatic = true;
