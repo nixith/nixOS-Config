b8c99200169003c8b7fa953adb9b10838ff50e84
nix <rwstauffer72@gmail.com>
2025-05-05 12:33:05 -0400

    change greetd to cli again

--- hosts/modules/greetd.nix
+++ hosts/modules/greetd.nix
@@ -1,17 +1,16 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   services.greetd = {
     enable = true;
-    # settings = {
-    #   default_session = {
-    #     command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time "
-    #       + "--cmd 'Hyprland' " + "--greeting 'Trans Rights' " + "--remember "
-    #       + "--remember-session " + "--user-menu";
-    #     user = "greeter";
-    #   };
-    # };
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time "
+          + "--cmd 'niri-session' " + "--greeting 'Trans Rights' "
+          + "--remember " + "--remember-session " + "--user-menu";
+        user = "greeter";
+      };
+    };
   };
-  programs.regreet = {
-    enable = true;
-  };
+  # programs.regreet = {
+  #   enable = true;
+  # };
 }
