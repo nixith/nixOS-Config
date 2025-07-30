378d8ea0f91067e757efe229ed9791ab4ef1be27
nixith <rwstauffer72@gmail.com>
2025-07-29 21:00:48 -0400

    fix fido enrollment option

--- hosts/laptop/disko.nix
+++ hosts/laptop/disko.nix
@@ -1,70 +1,75 @@
 {
   disko.devices = {
     disk = {
-      main = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted";
-                postCreateHook = # TODO: look into tpm2 enrollment
-                  ''
-                    systemd-cryptenroll --fido2-device=auto /dev/sda
-                  '';
-                # disable settings.keyFile if you want to use interactive password entry
-                passwordFile = "/tmp/secret.key"; # Interactive
-                settings = {
-                  allowDiscards = true;
-                  #keyFile = "/tmp/secret.key";
-                };
-                #additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+      main =
+        let
+          dev = "/dev/nvme0n1";
+        in
+
+        {
+          type = "disk";
+          device = dev;
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
                 content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "/root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" ];
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [
+                    "defaults"
+                  ];
+                };
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "crypted ";
+                  postCreateHook = # TODO: look into tpm2 enrollment
+                    ''
+                      systemd-cryptenroll --fido2-device=auto ${dev}
+                    '';
+                  # disable settings.keyFile if you want to use interactive password entry
+                  passwordFile = "/tmp/secret.key"; # Interactive
+                  settings = {
+                    allowDiscards = true;
+                    #keyFile = "/tmp/secret.key";
+                  };
+                  #additionalKeyFiles = [ "/tmp/additionalSecret.key" ];
+                  content = {
+                    type = "btrfs";
+                    extraArgs = [ "-f" ];
+                    subvolumes = {
+                      "/root" = {
+                        mountpoint = "/";
+                        mountOptions = [ "compress=zstd" ];
+                      };
+                      "/home" = {
+                        mountpoint = "/";
+                        mountOptions = [ "compress=zstd" ];
+                      };
+                      "/nix" = {
+                        mountpoint = "/nix";
+                        mountOptions = [
+                          "compress=zstd"
+                          "noatime"
+                        ];
+                      };
+                      # "/swap" = {
+                      #   mountpoint = "/.swapvol";
+                      #   swap.swapfile.size = "16G";
+                      # };
                     };
-                    "/home" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" ];
-                    };
-                    "/nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [
-                        "compress=zstd"
-                        "noatime"
-                      ];
-                    };
-                    # "/swap" = {
-                    #   mountpoint = "/.swapvol";
-                    #   swap.swapfile.size = "16G";
-                    # };
                   };
                 };
               };
             };
           };
         };
-      };
     };
   };
 }
