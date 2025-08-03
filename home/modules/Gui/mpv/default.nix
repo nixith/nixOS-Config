481d627d039ea968c382631799ae8537fd4d1552
nixith <rwstauffer72@gmail.com>
2025-08-03 14:40:38 -0400

    mpv youtube fixes

--- home/modules/Gui/mpv/default.nix
+++ home/modules/Gui/mpv/default.nix
@@ -13,6 +13,22 @@ in
   };
 
   config = lib.mkIf cfg.enable {
+
+    programs.aria2.enable = true;
+    programs.yt-dlp = {
+      enable = true;
+      settings = {
+        embed-thumbnail = true;
+        embed-subs = true;
+        sub-langs = "all";
+        downloader = "aria2c";
+        downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+      };
+    };
+    services.jellyfin-mpv-shim = {
+      enable = true;
+      mpvConfig = config.programs.mpv.config; # TODO: write dedicated config
+    };
     programs.mpv = {
       enable = true;
       scripts = with pkgs.mpvScripts; [
@@ -21,14 +37,19 @@ in
         thumbfast
         sponsorblock
         quality-menu
+        uosc
       ];
       config = {
         hwdec = "auto-safe";
         vo = "gpu";
         profile = "gpu-hq";
         gpu-context = "wayland";
-        ytdl-format = "bestvideo+bestaudio";
-        cache-default = 4000000;
+        demuxer-max-bytes = "512MiB";
+        demuxer-readahead-secs = 20;
+        ytdl-format = "bestvideo+bestaudio/best";
+        slang = "en";
+        sub-auto = "fuzzy";
+        ytdl-raw-options = "sub-lang=en,write-auto-sub=,write-subs=";
       };
     };
   };
