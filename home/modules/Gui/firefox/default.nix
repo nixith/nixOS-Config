b86d5666f30310573c9ba2947060e70013b6f94f
nixith <rwstauffer72@gmail.com>
2025-11-06 14:12:26 -0500

    firefox PWA

--- home/modules/Gui/firefox/default.nix
+++ home/modules/Gui/firefox/default.nix
@@ -1,11 +1,16 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    firefoxpwa
+    fx-cast-bridge
+  ];
   programs.firefox = {
     enable = true;
     nativeMessagingHosts = with pkgs; [
       tridactyl-native
       fx-cast-bridge
       ff2mpv
+      firefoxpwa
     ];
     package = pkgs.firefox;
     #TODO: make declarative profile
