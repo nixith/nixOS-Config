50412ce6cdba0fd4828c7f9de09572efc25fa5c1
nixith <rwstauffer72@gmail.com>
2026-03-24 16:55:48 -0400

    fix iwd not actually being enabled

--- hosts/modules/iwd.nix
+++ hosts/modules/iwd.nix
@@ -1,7 +1,12 @@
 { ... }:
 {
   networking = {
-    wireless.iwd.enable = true;
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        General.ControlPortOverNL80211 = false;
+      };
+    };
     networkmanager.wifi.backend = "iwd";
   };
   boot.kernelModules = [ "pkcs8_key_parser" ];
